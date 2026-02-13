c5cf6f0ad591ef5ba745734989b42d31aef8a387
levish0 <shiueo.csh@gmail.com>
2026-02-13 21:20:36 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './layout.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	let { children } = $props();
+</script>
+
+<ModeWatcher defaultMode="dark" />
+<Toaster />
+<div class=" min-h-screen max-w-screen">
+	<main>
+		{@render children()}
+	</main>
+</div>
